2aa50cc37dbec07fa8716872eafccd9edb937ab5
Khadija <khadijagulamhusein@gmail.com>
2024-03-27 12:24:20 +0000

    Changed function to take into calculate average and max of user inputs

--- while.py
+++ while.py
@@ -1,18 +1,27 @@
-# This programme is to calculate the average of user inputs as long as the input is not -1. Once -1 is hit the programme should break.
+# This programme is to calculate the average and maximum of user inputs as long as the input is not -1. Once -1 is hit the programme should break.
+
+user_input = int(input("Please input any number: "))
 
 # define the function
-def average_numbers():
+def statistics(user_input):
     count = 0
     updated_input = 0 
-    user_input = int(input("Please input any number: "))
+    numbers_list = []
     while user_input != -1:
+        #inputs for average
         updated_input += user_input
         count += 1 
         user_input = int(input("Please input any number: "))
         average = (updated_input / (count))
+
+        #inputs for max
+        numbers_list.append (user_input)
+        maximum = max(numbers_list)
+      
     print (f"The average of all numbers that are not -1 is {average}")
+    print (f"The maximum of all numbers that are not -1 is {maximum}")
 
 
-#run the function
-average_numbers()
-
+        
+#run the functions
+statistics(user_input)
